595497047468a3c344356d45a201c2f861b613f0
Artyom Vorozhbyanov <artyom.vorozhbyanov@gmail.com>
2024-07-16 15:54:44 +0200

    test black

--- src/app/api/routes/v1/healthcheck.py
+++ src/app/api/routes/v1/healthcheck.py
@@ -3,8 +3,6 @@ from fastapi import APIRouter, status, Response
 healthcheck_router = APIRouter()
 
 
-
-
 @healthcheck_router.get(
     "/healthcheck",
     status_code=status.HTTP_204_NO_CONTENT,
@@ -14,4 +12,6 @@ healthcheck_router = APIRouter()
     tags=["Healthcheck"],
 )
 async def healthcheck():
-    return Response(status_code=status.HTTP_200_OK)
+    return Response(
+        status_code=status.HTTP_200_OK
+    )
